775a46617f02a52493695da1b21e1ac85f144e58
manueldelgadocruz <manu.dlgd4@gmail.com>
2026-06-16 16:51:06 +0200

    Hito4 (I)
    paso de windows a mv

---
+++ DJF/ITs/it_asw_ccdronemng_freertos_hito4/Debug/llsw/freertos_osswr/Source/portable/ThirdParty/GCC/Posix/MemMang/heap_3.d
@@ -0,0 +1,20 @@
+llsw/freertos_osswr/Source/portable/ThirdParty/GCC/Posix/MemMang/heap_3.o: \
+ ../llsw/freertos_osswr/Source/portable/ThirdParty/GCC/Posix/MemMang/heap_3.c \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/FreeRTOS.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/FreeRTOSConfig.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/projdefs.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/portable.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/deprecated_definitions.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/portable/ThirdParty/GCC/Posix/portmacro.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/mpu_wrappers.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/task.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/list.h
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/FreeRTOS.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/FreeRTOSConfig.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/projdefs.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/portable.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/deprecated_definitions.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/portable/ThirdParty/GCC/Posix/portmacro.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/mpu_wrappers.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/task.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/freertos_osswr/Source/include/list.h:
