970e28cefcc3696d2802aafcb92ea7ea72a3c9b6
manueldelgadocruz <manu.dlgd4@gmail.com>
2026-07-01 20:03:33 +0200

    Hito final 3 (windows)
    Antes estaba mal

---
+++ DJF/FTs/ft_asw_uah_mars_drone_freertos/Debug/llsw/device_drv/src/emu_device_drv.d
@@ -0,0 +1,10 @@
+llsw/device_drv/src/emu_device_drv.o: \
+ ../llsw/device_drv/src/emu_device_drv.c \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/device_drv/include/public/device_drv.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/config/include/public/config.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/asw/edroom_glue/include/edroom_glue/edroomdf.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/config/include/public/basic_types.h
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/device_drv/include/public/device_drv.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/config/include/public/config.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/asw/edroom_glue/include/edroom_glue/edroomdf.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/config/include/public/basic_types.h:
---
+++ DJF/FTs/ft_asw_uah_mars_drone_freertos/Debug/service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.d
@@ -0,0 +1,14 @@
+service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.o: \
+ ../service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.c \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/service_libraries/pus_services/pus_sys_data_pool/include/public/pus_sys_data_pool.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/config/include/public/config.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/asw/edroom_glue/include/edroom_glue/edroomdf.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/config/include/public/basic_types.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/service_libraries/pus_services/pus_sys_data_pool/include/public/sys_data_pool_pids.h \
+ /home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/service_libraries/serialize/include/public/serialize.h
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/service_libraries/pus_services/pus_sys_data_pool/include/public/pus_sys_data_pool.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/config/include/public/config.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/asw/edroom_glue/include/edroom_glue/edroomdf.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/llsw/config/include/public/basic_types.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/service_libraries/pus_services/pus_sys_data_pool/include/public/sys_data_pool_pids.h:
+/home/atcsol/obdh_proy_26_Manuel_Delgado/DJF/ITs/it_asw_ccdronemng_freertos_hito3/service_libraries/serialize/include/public/serialize.h:
